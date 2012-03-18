b9de7e8f0599207d75098fee543a5fb94d0cd378
Mark Tomko <mtomko@strindberg.local>
2012-03-18 12:57:13 -0400

    cleaned up FlankingIndexSource and added spec

--- src/main/scala/org/marktomko/arfftools/polya/FlankingIndexSource.scala
+++ src/main/scala/org/marktomko/arfftools/polya/FlankingIndexSource.scala
@@ -6,7 +6,7 @@ object FlankingIndexSource {
   def indexesFor(lowerBound: Int, upperBound: Int) = {
     assert(lowerBound < 1)
     assert(upperBound > 0)
-    // skip over index 0
-    (lowerBound to -1) ++ (1 to upperBound)
+    // exclude 0
+    (lowerBound to upperBound) filterNot { _ == 0 }
   }
 }
---
+++ src/test/scala/org/marktomko/arfftools/polya/FlankingIndexSourceSpec.scala
@@ -0,0 +1,22 @@
+package org.marktomko.arfftools.polya
+
+import org.scalatest.Spec
+
+class FlankingIndexSourceSpec extends Spec {
+  describe ("Flanking Index Source") {
+
+    it ("should generate symmetric ranges from negative to positive, excluding 0") {
+      assert(Seq(-1, 1) === FlankingIndexSource.indexesFor(1).toIndexedSeq)
+    }
+
+    it ("should generate asymmetric ranges, excluding 0") {
+      assert(Seq(-5, -4, -3, -2, -1, 1) === FlankingIndexSource.indexesFor(-5, 1))
+    }
+
+    it ("should reject ranges not surrounding 0") {
+      intercept[AssertionError] {
+        FlankingIndexSource.indexesFor(1, -1)
+      }
+    }
+  }
+}
