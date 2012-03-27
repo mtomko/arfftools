c0fc84d0e17128f1afc184e60347435e624edaab
Mark Tomko <mtomko@broadinstitute.org>
2012-03-27 19:57:36 -0400

    improved CodonAtAttributeValueSource

--- src/main/scala/org/marktomko/arfftools/polya/CodonAtAttributeValueSource.scala
+++ src/main/scala/org/marktomko/arfftools/polya/CodonAtAttributeValueSource.scala
@@ -1,5 +1,8 @@
 package org.marktomko.arfftools.polya
 
+import scalaz.Scalaz._
+import org.marktomko.arfftools.arff.NominalAttribute
+
 /**
  * This attribute value source generates the codon (3 nucleotides)
  * at the specified codon index as its value. This assumes that the
@@ -12,4 +15,21 @@ class CodonAtAttributeValueSource(i: Int) extends IndexBasedAttributeValueSource
     assert(index < input.length / 3)
     input.substring(index * 3, (index + 1) * 3)
   }
+}
+
+object CodonAtAttributeValueSource {
+  private val bases = Set('A', 'C', 'G', 'T')
+
+  // scalaz magic from StackOverflow:
+  // http://stackoverflow.com/questions/7474709/all-combinations-with-repetition-using-scala
+  lazy val codons = bases.replicate[Seq](3).sequence map { _.mkString }
+
+  def sourcesFor(indexes: Iterable[Int]) = (indexes map { new CodonAtAttributeValueSource(_) })
+
+  def attributeFor(i: Int) =
+    NominalAttribute(
+      name = new StringBuilder("c_")
+        .append(if (i > 0) "plus_" else "minus_")
+        .append(math.abs(i)).toString(),
+      range = codons)
 }
